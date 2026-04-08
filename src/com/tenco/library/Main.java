4b13daa82c555b093aec6b65309c10e3f2f562e2
unknown <san1425865@gmail.com>
2026-04-08 10:33:12 +0900

    veiw 1차 코드 구현

--- src/com/tenco/library/Main.java
+++ src/com/tenco/library/Main.java
@@ -1,4 +1,10 @@
 package com.tenco.library;
 
+import com.tenco.library.view.LibraryView;
+
 public class Main {
+    public static void main(String[] args) {
+        LibraryView libraryView = new LibraryView();
+        libraryView.start();
+    }
 }
