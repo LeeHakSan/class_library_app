192246edf72bd3fa648b1ac85ab702a7925818c4
unknown <san1425865@gmail.com>
2026-04-08 16:26:41 +0900

    ê관리자 기능 처리

---
+++ src/com/tenco/library/dto/Admin.java
@@ -0,0 +1,18 @@
+package com.tenco.library.dto;
+
+
+import lombok.*;
+
+// 관리자 데이터를 받는 dto 클래스
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = "password") // toString 출력시 비밀번호 제외
+public class Admin {
+    private int id;
+    private String adminId;
+    private String password;
+    private String name;
+}
